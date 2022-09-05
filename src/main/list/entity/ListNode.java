4c646597af1e7126874da831498b0439b3bf6d11
wenzf <2822932334@qq.com>
2022-09-05 22:01:17 +0800

    单向链表类属性和方法优化

--- src/main/list/entity/ListNode.java
+++ src/main/list/entity/ListNode.java
@@ -3,16 +3,25 @@ package main.list.entity;
 /**
  * @Author: wenzf
  * @Date: 2022/08/25/20:59
- * @Description: 单向链表实体类
+ * @Description: 单向链表类
  */
 public class ListNode {
-    private int val;
+    public int val;
     public ListNode next = null;
     public ListNode(int val) {
         this.val = val;
     }
     public ListNode() {
     }
+    /**
+    * @Description: 链表长度属性
+    */
+    public static int length = 1;
+
+    /**
+     * @Description: 链表位置属性
+     */
+    public static int position = 1;
 
     /**
      * @Description: 添加一个节点
@@ -38,5 +47,37 @@ public class ListNode {
             //换行
             System.out.println();
         }
+    }
+
+    /***
+    * @Description: 获取链表长度
+    * @Param: []
+    * @return: int
+    */
+    public int getLength(){
+        if(this.next !=null){
+            length++;
+            this.next.getLength();
+        }
+        return length;
+    }
+    /***
+    * @Description: 获取链表值离头节点值最近的位置
+    * @Param: val：值;pos: 1 表示从头节点开始
+    * @return: int 返回位置
+    */
+    public int getPosition(int val,int pos){
+        if (pos == 1) {
+            if (this.next != null) {
+                if (this.val != val) {
+                    position++;
+                    this.next.getPosition(val,1);
+                }
+            }
+        }else{
+            position = 1;
+            this.next.getPosition(val,1);
+        }
+        return position;
     }
 }
